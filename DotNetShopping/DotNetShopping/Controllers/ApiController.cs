bc424b322eadcee98c88b78b864bb57e07d5b071
Fatih Cinar <fatihcinar96@gmail.com>
2019-04-16 18:12:40 +0300

    automatic load more on product page has builded.

--- DotNetShopping/DotNetShopping/Controllers/ApiController.cs
+++ DotNetShopping/DotNetShopping/Controllers/ApiController.cs
@@ -134,5 +134,72 @@ namespace DotNetShopping.Controllers
                 }).ToList();
             return Json(new { Success = true, results = model }, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult LoadMoreProducts(Int16? Category, Decimal? min, Decimal? max, Int16? BrandId, Int16 count)
+        {
+            Category = Category ?? 0;
+            if (Category > 0)
+            {
+                var SelectedCategory = db.Categories.Find(Category);
+                ViewBag.SelectedCategory = SelectedCategory;
+            }
+            min = min ?? 0;
+            ViewBag.Min = min ?? 0;
+            max = max ?? 1000;
+            ViewBag.Max = max ?? 1000;
+            BrandId = BrandId ?? 0;
+            ViewBag.Brand = BrandId ?? 0;
+            ViewBag.SelectedBrand = BrandId == 0;
+            try
+            {
+                var productsQuery = db.Variants.Include("Product").Include("Brand")
+                .Where(x => x.Archived == false && x.Product.Archived == false
+                && x.IsVisible == true && x.Stock > 0 && x.Product.OnSale == true)
+                .Join(db.Categories, v => v.Product.CategoryId,
+                c => c.CategoryId, (v, c) => new { Variant = v, Category = c });
+
+                if (Category > 0)
+                {
+                    productsQuery = productsQuery.Where(x => x.Category.CategoryId == Category || x.Category.ParentId == Category);
+                }
+
+                productsQuery = productsQuery.Where(x => x.Variant.UnitPrice > min && x.Variant.UnitPrice < max);
+                if (BrandId > 0)
+                {
+                    productsQuery = productsQuery.Where(x => x.Variant.Product.BrandId == BrandId);
+                }
+
+                var products = productsQuery.OrderByDescending(x => x.Variant.CreateDate).Skip(count * 12)
+                .Take(12).Select(x => new ProductBoxModel
+                {
+                    ProductId = x.Variant.ProductId,
+                    VariantId = x.Variant.VariantId,
+                    ProductName = x.Variant.Product.Name,
+                    VariantName = x.Variant.Name,
+                    BrandName = x.Variant.Product.Brand.Name,
+                    CategoryName = x.Category.Name,
+                    UnitPrice = x.Variant.UnitPrice,
+                    PhotoName = db.ProductImages
+                    .Where(i => i.VariantId == x.Variant.VariantId)
+                    .OrderBy(i => i.Sequence).FirstOrDefault().FileName
+                })
+                .ToList();
+                ViewBag.Products = products;
+
+                var categories = db.Categories.Where(x => x.ParentId == (Category ?? 0))
+                    .OrderBy(x => x.Name)
+                    .ToList();
+                var brands = db.Brands.OrderBy(x => x.Name).ToList();
+                ViewBag.Brands = brands;
+                ViewBag.Categories = categories;
+                return PartialView("LoadMoreProductsPartialView", products);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                ViewBag.Products = new List<ProductBoxModel>();
+            }
+            return View();
+        }
     }
 }
---
+++ DotNetShopping/DotNetShopping/Scripts/Products.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+
+            var dataToPost = {
+                min: minValue,
+                max: maxValue,
+                count: ++count,
+                BrandId: brandId,
+                Category: category
+            };
+            $.post('/Api/LoadMoreProducts', dataToPost)
+                .done(function (response, status, jqxhr) {
+                    $(".ProductList").append(response);
+                })
+                .fail(function (error, status, jqxhr) {
+                    alert("something went wrong");
+                })
+        }
+    });
+})
+
